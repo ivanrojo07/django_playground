cf337394c65ee7d0e6631977c05e89d9b0602356
Ivan Rojo <ivanrojo07@gmail.com>
2018-10-01 22:23:50 -0500

    create view

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PageListView, PageDetailView
-urlpatterns = [
+from .views import PageListView, PageDetailView, PageCreate
+pages_patterns = ([
     # path('', views.pages, name='pages'),
     path('', PageListView.as_view(), name='pages'),
     # path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
-]
+    path('create/', PageCreate.as_view(), name="create"),
+], 'pages')
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,25 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse, reverse_lazy
 
 class PageListView(ListView):
     model = Page
 
 class PageDetailView(DetailView):
     model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    fields=[
+        'title',
+        'content',
+        'order'
+    ]
+    success_url = reverse_lazy('pages:pages')
+    # def get_success_url(self):
+    #     return reverse('pages:pages')
 # Create your views here.
 # def pages(request):
 #     pages = get_list_or_404(Page)
